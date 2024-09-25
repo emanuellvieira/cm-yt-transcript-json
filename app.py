0b50d236fd63abd54a8aac31627662bb25268184
Emanuell Vieira <42053162+emanuellvieira@users.noreply.github.com>
2024-09-25 00:10:43 -0300

    add lang selector and error fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._errors import NoTranscriptFound, TranscriptsDisabled
 import json
 import re
 import os
@@ -14,10 +15,16 @@ def extract_video_id(url):
     return None
 
 # Função para extrair a transcrição do YouTube
-def get_transcription(video_id, language='pt'):
+def get_transcription(video_id, language='en'):
     try:
         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[language])
         return transcript
+    except NoTranscriptFound:
+        print(f"Transcrição não disponível no idioma '{language}' para o vídeo.")
+        return None
+    except TranscriptsDisabled:
+        print("As transcrições estão desativadas para este vídeo.")
+        return None
     except Exception as e:
         print(f"Erro ao obter a transcrição: {e}")
         return None
@@ -27,10 +34,11 @@ def get_transcription(video_id, language='pt'):
 def index():
     if request.method == 'POST':
         video_url = request.form['video_url']
+        language = request.form.get('language', 'en')  # Pega o idioma do formulário
         video_id = extract_video_id(video_url)
         
         if video_id:
-            transcript = get_transcription(video_id)
+            transcript = get_transcription(video_id, language)
             if transcript:
                 output_file = f"transcription_{video_id}.json"
                 with open(output_file, 'w', encoding='utf-8') as json_file:
@@ -38,7 +46,7 @@ def index():
                 
                 return send_file(output_file, as_attachment=True)
             else:
-                return "Nenhuma transcrição encontrada para o vídeo."
+                return "Nenhuma transcrição encontrada para o vídeo. Pode ser que as transcrições estejam desativadas ou o idioma não esteja disponível."
         else:
             return "URL inválida. Verifique a URL do vídeo."
     
